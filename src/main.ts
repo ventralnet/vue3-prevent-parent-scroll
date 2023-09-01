d3cd1b8ad70aa78104dd086168f526ac9afda7dd
Matthew Kirkley <matt.kirkley@gmail.com>
2023-09-01 16:06:07 +0000

    Vue3 plugin installation guidelines

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,28 @@
 const map = new Map<HTMLElement, EventListenerOrEventListenerObject>()
 
-export function install(Vue) {
-  Vue.directive('prevent-parent-scroll', {
-    beforeMount(el: HTMLElement) {
-      const handler = (event: WheelEvent) => {
-        if (el.scrollTop === 0  && event.deltaY < 0 ||
-            Math.abs(el.scrollTop - (el.scrollHeight - el.clientHeight)) <= 1 &&
-            event.deltaY > 0)
-        {
-          event.preventDefault()
-        }
+const directive = {
+  beforeMount(el: HTMLElement) {
+    const handler = (event: WheelEvent) => {
+      if (el.scrollTop === 0  && event.deltaY < 0 ||
+          Math.abs(el.scrollTop - (el.scrollHeight - el.clientHeight)) <= 1 &&
+          event.deltaY > 0)
+      {
+        event.preventDefault()
       }
-      map.set(el, handler)
-      el.addEventListener('wheel', handler)
-    },
-    unmounted(el: HTMLElement) {
-      el.removeEventListener('wheel', map.get(el))
-      map.delete(el)
     }
-  })
-}
+    map.set(el, handler)
+    el.addEventListener('wheel', handler)
+  },
+  unmounted(el: HTMLElement) {
+    el.removeEventListener('wheel', map.get(el))
+    map.delete(el)
+  }
+};
+
+const plugin = {
+  install(app, options) {
+    app.directive('preventParentScroll', directive);
+  },
+};
+
+export default plugin;
